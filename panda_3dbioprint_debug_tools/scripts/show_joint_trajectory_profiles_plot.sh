89dcd6e60965f940071a9e6b33607cd6d7e7966f
Ricardo Tonet <ribeiro.tonet@gmail.com>
2020-03-05 02:03:55 +0000

    First commit of new development route.

--- panda_3dbioprint_debug_tools/scripts/show_joint_trajectory_profiles_plot.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-kst2 ~/ROS/tese/src/panda_3dbioprint_debug_tools/data/single_joint_profile.dat --xlabel "Time (s)" -x 1 --ylabel "Position (rad)" -y 2 --ylabel "Velocity (rad/s)" -y 3 --ylabel "Acceleration (rad/s^2)" -y 4;
